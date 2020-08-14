722635285b3b1cb5108a5eefcb142ce513e2e227
EllisonChen <69653211+EllisonChen@users.noreply.github.com>
2020-08-14 12:57:20 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main 
+{
+	 public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("My name is Ellison");
+		System.out.println("I enjoy playing badminton");
+		System.out.println("My favorite videogame is Minecraft");
+		System.out.println("I like science");
+		System.out.println("My favorite food is steak");
+	 }
+}
+ 
